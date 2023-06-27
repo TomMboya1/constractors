0975d00809f60ee49bdb259b5b39f9d21f83b712
Tom MBOYA <tommboya044@gmail.com>
2023-06-27 10:44:34 +0300

    1st commit

---
+++ src/main/java/org/example/house.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class house {
+    private String address;
+    private int numberOfFloors;
+
+    public house(String address, int numberOfFloors) {
+        this.address = address;
+        this.numberOfFloors = numberOfFloors;
+        System.out.println("A House object has been created.");
+    }
+    // Getters and setters (if needed) can be added here
+
+    // Other methods can be added here
+}
---
+++ src/main/java/org/example/tester.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class tester {
+    public static void main(String[] args) {
+        house house1 = new house("123 Main Street", 2);
+        house house2 = new house("456 Elm Avenue", 3);
+    }
+}
